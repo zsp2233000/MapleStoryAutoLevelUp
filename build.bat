09a3d1da67d275515537dca840d59e48a12e9ac2
Ken Yu <luckyyu910645@gmail.com>
2025-07-22 14:07:44 +0800

    [Dev] Add icon for release file
    Signed-off-by: Ken Yu <luckyyu910645@gmail.com>

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconsole --onefile src/main.py -p . -n MapleStoryAutoLevelUp --hidden-import=pkg_resources.py2_warn --hidden-import=pkg_resources.extern
+pyinstaller --noconsole --onefile src/main.py -p . --icon=media/icon.ico -n MapleStoryAutoLevelUp --hidden-import=pkg_resources.py2_warn --hidden-import=pkg_resources.extern
